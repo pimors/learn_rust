98abdc50dc116e89bc94d3d274f751cf8b7e58ed
pimors <ffej@DESKTOP-CV1IDP8.localdomain>
2021-03-11 12:22:20 +0100

    second commit

---
+++ guessing_game/target/debug/deps/guessing_game-5ac6566481573f12.d
@@ -0,0 +1,5 @@
+/home/ffej/fun/learn_rust/guessing_game/target/debug/deps/guessing_game-5ac6566481573f12: src/main.rs
+
+/home/ffej/fun/learn_rust/guessing_game/target/debug/deps/guessing_game-5ac6566481573f12.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/ffej/fun/learn_rust/guessing_game/target/debug/guessing_game: /home/ffej/fun/learn_rust/guessing_game/src/main.rs
