f4bcb331d4ea93f6bf3e28cbcf6bd5da4ed31284
pimors <idc@nightmare.life>
2021-03-11 13:00:14 +0100

    guessing game finished

--- guessing_game/target/debug/guessing_game.d
+++ guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/home/ffej/fun/learn_rust/guessing_game/target/debug/guessing_game: /home/ffej/fun/learn_rust/guessing_game/src/main.rs
+/home/ffej/fun/learn_rust/guessing_game/target/debug/guessing_game: /home/ffej/fun/learn_rust/guessing_game/src/main.rs build.rs
